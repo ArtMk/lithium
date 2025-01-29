e78e38c73c5f16c4a3618b2fa4ee7e58b05b361a
artak <artak.mkrtchyan@gmail.com>
2025-01-29 14:34:27 +0100

    Changed output units of diagnostics to SI units

--- li/diagnostic.py
+++ li/diagnostic.py
@@ -51,23 +51,23 @@ def U_dip(wavelength, power, waist, from_trap_freq, trap_freq = 0):
         it is only valid for atoms that have very close excited states like is the case for lithium 6 (D1, D2 transitions)
 
     Arguments:
-        wavelength     -- {scalar} laser wavelength [nm]
+        wavelength     -- {scalar} laser wavelength [m]
         power          -- {scalar} laser power [W]
         waist          -- {scalar} laser waist/beam width [m]
         from_trap_freq -- {bool} calculate dipole potential from trap frequency
         trap_freq      -- {scalar} trap frequency [Hz]
 
     Returns:
-        {scalar} dipole potential [nk]
+        {scalar} dipole potential [K]
         or
         {scalar} dipole potential [J]
     """
 
-    f_laser = 2 * const.pi * const.c / (wavelength * 1e-9)
+    f_laser = 2 * const.pi * const.c / wavelength
     D2_detuning = np.abs(f_laser - f_res)
 
     if from_trap_freq:
-        return m_Li * waist**2 * const.pi**2 * trap_freq**2 / const.k * 1e9
+        return m_Li * waist**2 * const.pi**2 * trap_freq**2 / const.k
 
     else:
         return 3 * const.pi * const.c**2 / (2 * f_res**3) * Gamma / D2_detuning * intensity(power, waist)
@@ -81,7 +81,7 @@ def trap_freq(wavelength, power, waist, from_trap_freq = False):
         It is realized by specifying the resonant frequency of the transition, the line width of the transition and the mass of atomic lithium 6.
 
     Arguments:
-        wavelength     -- {scalar} laser wavelength [nm]
+        wavelength     -- {scalar} laser wavelength [m]
         power          -- {scalar} laser power [W]
         waist          -- {scalar} laser waist/beam width [m]
         from_trap_freq -- {bool} leave to False
@@ -103,7 +103,7 @@ def breit_rabi(B, state): # B in Gaus, return in MHz
         state -- {integer} hyperfine state
 
     Returns:
-        {scalar} something, not sure yet
+        {scalar} some frequency, idk [Hz]
 
     """
 
@@ -123,7 +123,7 @@ def breit_rabi(B, state): # B in Gaus, return in MHz
     else:
         root = 1 + m_F / abs(m_F) * a
 
-    return (-vHFS / (2 * (2 * I + 1)) + g_I * m_F * mu_B * B * 1e-4 / h + vHFS * (F - 1) * root) * 1e-6
+    return (-vHFS / (2 * (2 * I + 1)) + g_I * m_F * mu_B * B * 1e-4 / h + vHFS * (F - 1) * root)
 
 
 def get_F(state):
